722ba60b51bab684c1fc27b86e2a962103854fed
NotFieldgamer <fieldgamer789@gmail.com>
2026-01-04 21:10:38 +0530

    Login/Register fix v2

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const API = `${import.meta.env.VITE_API_BASE_URL}/api/auth`;
+const API = `${import.meta.env.VITE_API_BASE_URL}`;
 
 export const registerUser = (data) =>
-  axios.post(`${API}/register`, data);
+  axios.post(`${API}/api/auth/register`, data);
 
 export const loginUser = (data) =>
-  axios.post(`${API}/login`, data);
+  axios.post(`${API}/api/auth/login`, data);
 
 export const verifyToken = (token) =>
   axios.get(`${API}/verify`, {
